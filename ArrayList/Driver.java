4c2808efc25c2fe223f90aba357cbbf02f1115f1
Johnny So <jso123450@gmail.com>
2014-11-12 09:25:47 -0500

    ArrayList

---
+++ ArrayList/Driver.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Driver{
+
+    public void removeDup(ArrayList<Integer> array){
+	int holder = array[0];
+	for (int = 0; i < array.length; i++){
+	    if (array[i] == array[i+1])
+		array.remove[i+1];
+	}
+    }
+
+    public static void main(String[] args){
+	// need to specify what type of objects al will store
+	ArrayList<String> al;
+	al = new ArrayList<String>();
+	System.out.println(al);
+	al.add("hello");
+	System.out.println(al);
+	al.add("world");
+	System.out.println(al);
+	al.add("33.2");
+	System.out.println(al);
+
+    ArrayList<Integer> ai = new ArrayList<Integer>();
+    Random r = new Random();
+    for (int i =0; i < 20; i++){
+	ai.add(r.nextInt(5));;
+    }
+    System.out.println(ai);
+    removeDup(ai);
+    System.out.println(ai);
+    
+
+    }
+    
+}
