090f1c8bdc4d7cc5c722f7c4a23b385c8a9d5951
Johnny So <jso123450@gmail.com>
2015-02-09 08:37:52 -0500

    Start of the Maze; from classcode/shared

--- Recursion/feb5.java
+++ Recursion/feb5.java
@@ -7,9 +7,20 @@ public class feb5{
 	}
     }
 
+    public int fact(int n){
+	if (n == 0)
+	    return 1;
+	else if (n > 0){
+	    return (n * fact(n-1));
+	}
+	return 0;
+    }
+
     public static void main(String[] args){
 	feb5 test = new feb5();
 	test.printme(5);
+	test.fact(5);
+	test.fact(-5);
     }
 
 }
