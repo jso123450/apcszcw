e676192689b6c7f986afb788e1f9d5bb15b78342
Johnny So <jso123450@gmail.com>
2015-05-18 08:39:55 -0400

    notes

--- Heap/minHeap.java
+++ Heap/minHeap.java
@@ -5,9 +5,11 @@
   - parent < children
   - full as possible, left to right
   - find min: O(1)
+    convert   O(n)
+      // converting from array to heap
     removeMin O(lg(n))
     pushdown  O(lg(n))
-    heapSort  O(n lg(n))
+    heapSort  O(n*lg(n))
     insert    O(lg(n))
 
   insert
@@ -20,7 +22,11 @@
      swap the root with its smaller child and
      continue down until it complies with the
      properties of the min Heap
-  a
-
+  
+  heapSort
+  1) swap first & last nodes
+     last index -= 1
+  2) pushdown until it complies
+  3) repeat
 
  */
